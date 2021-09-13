eb0649c8a41ce74c1622809a25f61813835f66f9
A Aida <wasnot@users.noreply.github.com>
2021-09-13 11:46:57 +0900

    fix: add error handling. (#150)

--- gradle-plugin/src/main/kotlin/io/karte/android/gradleplugin/visualtracking/ModificationLambdaSpecification.kt
+++ gradle-plugin/src/main/kotlin/io/karte/android/gradleplugin/visualtracking/ModificationLambdaSpecification.kt
@@ -4,8 +4,14 @@ import javassist.CtClass
 import javassist.CtMethod
 
 object ModificationLambdaSpecification {
+    private fun isLambda(methodName: String) = methodName.contains(Regex("\\\$lambda-[0-9]"))
+    private fun isViewProcessor(method: CtMethod): Boolean {
+        return runCatching {
+            method.parameterTypes.map(CtClass::getName).contains("android.view.View")
+        }.getOrDefault(false)
+    }
+
     fun isSatisfied(method: CtMethod): Boolean {
-        return method.parameterTypes.map(CtClass::getName).contains("android.view.View") &&
-            method.name.contains(Regex("\\\$lambda-[0-9]"))
+        return isViewProcessor(method) && isLambda(method.name)
     }
 }
